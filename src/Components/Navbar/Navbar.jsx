1c821e8bfe33b372a37e03dfe6ca84fdd570a9e3
Sara Khushi <sarakhushi0347@gmail.com>
2026-04-10 13:47:02 +0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useContext } from 'react';
 import { ShopContext } from '../../Context/ShopContext';
-
 import './Navbar.css';
 import logo from '../assets/image.png';
 import cart_icon from "../assets/cart_icon.png";
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
   const [menu, setMenu] = useState("shop")
   const [toggle, setToggle] = useState(false)
 
@@ -14,48 +14,69 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      
+
+      {/* Logo */}
       <div className="nav-logo">
         <img src={logo} alt="Logo" />
         <p>SHOPPER</p>
       </div>
 
-      {/* Toggle Button */}
-      <div className={`nav-toggle ${toggle ? "open" : ""}`} onClick={()=>setToggle(!toggle)}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-
+      {/* Menu */}
       <ul className={`nav-menu ${toggle ? "active" : ""}`}>
+        
         <li onClick={()=>{setMenu("shop"); setToggle(false)}}>
           <Link to="/">Shop</Link>
-          {menu==="shop"?<hr/>:<></>}
+          {menu==="shop"?<hr/>:null}
         </li>
 
         <li onClick={()=>{setMenu("mens"); setToggle(false)}}>
           <Link to="/mens">Men</Link>
-          {menu==="mens"?<hr/>:<></>}
+          {menu==="mens"?<hr/>:null}
         </li>
 
         <li onClick={()=>{setMenu("womens"); setToggle(false)}}>
           <Link to="/womens">Women</Link>
-          {menu==="womens"?<hr/>:<></>}
+          {menu==="womens"?<hr/>:null}
         </li>
 
         <li onClick={()=>{setMenu("kids"); setToggle(false)}}>
           <Link to="/kids">Kids</Link>
-          {menu==="kids"?<hr/>:<></>}
+          {menu==="kids"?<hr/>:null}
         </li>
+
       </ul>
 
+      {/* Login + Cart */}
       <div className="nav-login-cart">
-        <Link to="/login"><button>Login</button></Link> 
-        <Link to="/cart"><img src={cart_icon} alt="" /></Link>
-        <div className='nav-cart-count'>{getTotalCartItems()}</div>
+
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+
+        <div className="cart-icon">
+          <Link to="/cart">
+            <img src={cart_icon} alt="" />
+          </Link>
+
+          <div className='nav-cart-count'>
+            {getTotalCartItems()}
+          </div>
+        </div>
+
       </div>
+
+      {/* Toggle */}
+      <div 
+        className={`nav-toggle ${toggle ? "open" : ""}`} 
+        onClick={()=>setToggle(!toggle)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
